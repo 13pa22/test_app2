d6184592b73cea2d82aa5637ccd85b0a099d86ca
13pa22 <32932581+13pa22@users.noreply.github.com>
2019-08-07 23:26:01 -0400

    Updates

---
+++ precache-manifest.72c27ac2ac546449dd59a0c7a0775653.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb8886b114b05d7bb3e03dd8a322095",
+    "url": "/test_app2/index.html"
+  },
+  {
+    "revision": "43beb4689dff68018535",
+    "url": "/test_app2/static/js/2.e7dbb282.chunk.js"
+  },
+  {
+    "revision": "8c43da2acec3e2fa7b4f",
+    "url": "/test_app2/static/js/main.a169d072.chunk.js"
+  },
+  {
+    "revision": "d46b68e018ae18743610",
+    "url": "/test_app2/static/js/runtime~main.d0442b57.js"
+  }
+]);
